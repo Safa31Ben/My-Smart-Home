60425cc05555bedfa5582188ef31320be67f5364
Safa benabdessadok <safa.benabdessadok@univ-constantine2.dz>
2023-04-04 09:51:19 +0100

    Personne component

---
+++ Personne/PersonneGUI.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace Personne
+{
+    partial class PersonneGUI
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PersonneGUI));
+            this.idLabel = new System.Windows.Forms.Label();
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.accessBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.msgBox = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Températurelabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tempBox = new System.Windows.Forms.TextBox();
+            this.humiBox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.minimizeBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idLabel.Location = new System.Drawing.Point(20, 95);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(223, 28);
+            this.idLabel.TabIndex = 0;
+            this.idLabel.Text = "Nom de utilisateur";
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameBox.Location = new System.Drawing.Point(205, 92);
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.Size = new System.Drawing.Size(243, 37);
+            this.usernameBox.TabIndex = 1;
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordBox.Location = new System.Drawing.Point(205, 149);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(243, 37);
+            this.passwordBox.TabIndex = 3;
+            this.passwordBox.UseSystemPasswordChar = true;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.Location = new System.Drawing.Point(20, 149);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(155, 28);
+            this.passwordLabel.TabIndex = 2;
+            this.passwordLabel.Text = "Mot de pass";
+            // 
+            // accessBtn
+            // 
+            this.accessBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(112)))), ((int)(((byte)(152)))));
+            this.accessBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(112)))), ((int)(((byte)(152)))));
+            this.accessBtn.FlatAppearance.BorderSize = 2;
+            this.accessBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.accessBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.accessBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.accessBtn.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.accessBtn.Location = new System.Drawing.Point(191, 197);
+            this.accessBtn.Name = "accessBtn";
+            this.accessBtn.Padding = new System.Windows.Forms.Padding(2);
+            this.accessBtn.Size = new System.Drawing.Size(97, 41);
+            this.accessBtn.TabIndex = 4;
+            this.accessBtn.Text = "Access";
+            this.accessBtn.UseVisualStyleBackColor = false;
+            this.accessBtn.Click += new System.EventHandler(this.accessBtn_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 300);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 21);
+            this.label1.TabIndex = 5;
+            // 
+            // msgBox
+            // 
+            this.msgBox.BackColor = System.Drawing.Color.White;
+            this.msgBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.msgBox.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msgBox.Location = new System.Drawing.Point(0, 255);
+            this.msgBox.Multiline = true;
+            this.msgBox.Name = "msgBox";
+            this.msgBox.ReadOnly = true;
+            this.msgBox.Size = new System.Drawing.Size(478, 26);
+            this.msgBox.TabIndex = 6;
+            this.msgBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Personne.Properties.Resources.humidite_32;
+            this.pictureBox2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.pictureBox2.Location = new System.Drawing.Point(246, 298);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 19;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = global::Personne.Properties.Resources.temperature_32;
+            this.pictureBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 298);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Températurelabel
+            // 
+            this.Températurelabel.AutoSize = true;
+            this.Températurelabel.BackColor = System.Drawing.Color.Transparent;
+            this.Températurelabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Températurelabel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Températurelabel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Températurelabel.Location = new System.Drawing.Point(52, 304);
+            this.Températurelabel.Name = "Températurelabel";
+            this.Températurelabel.Padding = new System.Windows.Forms.Padding(3);
+            this.Températurelabel.Size = new System.Drawing.Size(165, 34);
+            this.Températurelabel.TabIndex = 15;
+            this.Températurelabel.Text = "Température";
+            this.Températurelabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label2.Location = new System.Drawing.Point(280, 304);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(3);
+            this.label2.Size = new System.Drawing.Size(121, 34);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Humidité";
+            // 
+            // tempBox
+            // 
+            this.tempBox.BackColor = System.Drawing.Color.White;
+            this.tempBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tempBox.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tempBox.Location = new System.Drawing.Point(154, 307);
+            this.tempBox.Name = "tempBox";
+            this.tempBox.ReadOnly = true;
+            this.tempBox.Size = new System.Drawing.Size(89, 27);
+            this.tempBox.TabIndex = 14;
+            this.tempBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // humiBox
+            // 
+            this.humiBox.BackColor = System.Drawing.Color.White;
+            this.humiBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.humiBox.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.humiBox.Location = new System.Drawing.Point(365, 305);
+            this.humiBox.Name = "humiBox";
+            this.humiBox.ReadOnly = true;
+            this.humiBox.Size = new System.Drawing.Size(113, 27);
+            this.humiBox.TabIndex = 17;
+            this.humiBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(112)))), ((int)(((byte)(152)))));
+            this.panel1.Controls.Add(this.minimizeBtn);
+            this.panel1.Controls.Add(this.closeBtn);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(478, 38);
+            this.panel1.TabIndex = 20;
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(53)))), ((int)(((byte)(106)))));
+            this.closeBtn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.closeBtn.FlatAppearance.BorderSize = 0;
+            this.closeBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(152)))), ((int)(((byte)(181)))));
+            this.closeBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(152)))), ((int)(((byte)(181)))));
+            this.closeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeBtn.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold);
+            this.closeBtn.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.closeBtn.Location = new System.Drawing.Point(435, 0);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(43, 38);
+            this.closeBtn.TabIndex = 0;
+            this.closeBtn.Text = "X";
+            this.closeBtn.UseVisualStyleBackColor = false;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label3.Location = new System.Drawing.Point(14, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(229, 34);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "My Smart Home";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // minimizeBtn
+            // 
+            this.minimizeBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(53)))), ((int)(((byte)(106)))));
+            this.minimizeBtn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.minimizeBtn.FlatAppearance.BorderSize = 0;
+            this.minimizeBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(152)))), ((int)(((byte)(181)))));
+            this.minimizeBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(152)))), ((int)(((byte)(181)))));
+            this.minimizeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimizeBtn.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minimizeBtn.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.minimizeBtn.Location = new System.Drawing.Point(392, 0);
+            this.minimizeBtn.Name = "minimizeBtn";
+            this.minimizeBtn.Size = new System.Drawing.Size(43, 38);
+            this.minimizeBtn.TabIndex = 1;
+            this.minimizeBtn.Text = "_";
+            this.minimizeBtn.UseVisualStyleBackColor = false;
+            this.minimizeBtn.Click += new System.EventHandler(this.minimizeBtn_Click);
+            // 
+            // PersonneGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(478, 348);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Températurelabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tempBox);
+            this.Controls.Add(this.humiBox);
+            this.Controls.Add(this.msgBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.accessBtn);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameBox);
+            this.Controls.Add(this.idLabel);
+            this.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PersonneGUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Habitant";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.TextBox usernameBox;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Button accessBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox msgBox;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Températurelabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tempBox;
+        private System.Windows.Forms.TextBox humiBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button minimizeBtn;
+    }
+}
+
---
+++ Personne/PersonneGUI.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Server_bib;
+using Smart_Home_bib;
+using Système_de_sécurité_bib;
+using Système_de_surveillance_bib;
+
+namespace Personne
+{
+    public partial class PersonneGUI : Form
+    {
+        public PersonneGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void accessBtn_Click_1(object sender, EventArgs e)
+        {
+            DemanderAcces o = (DemanderAcces)Activator.GetObject(typeof(DemanderAcces), 
+                "tcp://localhost:8088/Demander acces");
+            Compte compte = o.DemanderAcces(this.usernameBox.Text, this.passwordBox.Text);
+
+            if (compte != null)
+            {
+                this.msgBox.Text = "Bienvenu " + compte.Prenom + " " + compte.Nom;
+                this.usernameBox.Clear();
+                this.passwordBox.Clear();
+              
+                GestionAcces oha = (GestionAcces)Activator.GetObject(typeof(GestionAcces),
+                "tcp://localhost:8090/Server");
+                oha.enregisterAcces(new HistoriqueAccess( compte.Nom, compte.Prenom));
+
+                consulterTemp();
+            }
+            else
+            {
+                this.msgBox.Text = "Les information sont inccorect";
+                this.humiBox.Clear();
+                this.tempBox.Clear();
+            }
+            
+        }
+
+        async void consulterTemp () {
+            ConsulterHumidite oh = (ConsulterHumidite)Activator.GetObject(typeof(ConsulterHumidite),
+               "tcp://localhost:8085/température et humidité");
+            ConsulterTempérature ot = (ConsulterTempérature)Activator.GetObject(typeof(ConsulterTempérature),
+                    "tcp://localhost:8085/température et humidité");
+            
+            while (true)
+            {
+                this.humiBox.Text = oh.getHumidite().ToString() + " %";
+                this.tempBox.Text = ot.getTemperature().ToString() + " °C";  
+                await Task.Delay(500);
+            }
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void minimizeBtn_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
