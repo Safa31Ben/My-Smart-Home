3e833f667e6f5b8183980ae2d079dd1a979f110b
Safa benabdessadok <safa.benabdessadok@univ-constantine2.dz>
2023-04-04 09:52:11 +0100

    Porte component

---
+++ Porte/Porte.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Porte
+{
+    partial class Porte
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Porte));
+            this.nomLabel = new System.Windows.Forms.Label();
+            this.nomBox = new System.Windows.Forms.TextBox();
+            this.prenomBox = new System.Windows.Forms.TextBox();
+            this.prenomLabel = new System.Windows.Forms.Label();
+            this.accessBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.msgBox = new System.Windows.Forms.TextBox();
+            this.minimizeBtn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nomLabel
+            // 
+            this.nomLabel.AutoSize = true;
+            this.nomLabel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nomLabel.Location = new System.Drawing.Point(37, 87);
+            this.nomLabel.Name = "nomLabel";
+            this.nomLabel.Size = new System.Drawing.Size(68, 28);
+            this.nomLabel.TabIndex = 0;
+            this.nomLabel.Text = "Nom";
+            // 
+            // nomBox
+            // 
+            this.nomBox.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nomBox.Location = new System.Drawing.Point(135, 82);
+            this.nomBox.Name = "nomBox";
+            this.nomBox.Size = new System.Drawing.Size(211, 37);
+            this.nomBox.TabIndex = 1;
+            // 
+            // prenomBox
+            // 
+            this.prenomBox.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.prenomBox.Location = new System.Drawing.Point(135, 149);
+            this.prenomBox.Name = "prenomBox";
+            this.prenomBox.Size = new System.Drawing.Size(211, 37);
+            this.prenomBox.TabIndex = 3;
+            // 
+            // prenomLabel
+            // 
+            this.prenomLabel.AutoSize = true;
+            this.prenomLabel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.prenomLabel.Location = new System.Drawing.Point(37, 153);
+            this.prenomLabel.Name = "prenomLabel";
+            this.prenomLabel.Size = new System.Drawing.Size(100, 28);
+            this.prenomLabel.TabIndex = 2;
+            this.prenomLabel.Text = "Prenom";
+            // 
+            // accessBtn
+            // 
+            this.accessBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(112)))), ((int)(((byte)(152)))));
+            this.accessBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(112)))), ((int)(((byte)(152)))));
+            this.accessBtn.FlatAppearance.BorderSize = 2;
+            this.accessBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.accessBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.accessBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.accessBtn.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.accessBtn.ForeColor = System.Drawing.Color.Black;
+            this.accessBtn.Location = new System.Drawing.Point(97, 204);
+            this.accessBtn.Name = "accessBtn";
+            this.accessBtn.Size = new System.Drawing.Size(200, 43);
+            this.accessBtn.TabIndex = 4;
+            this.accessBtn.Text = "Demander access";
+            this.accessBtn.UseVisualStyleBackColor = false;
+            this.accessBtn.Click += new System.EventHandler(this.accessBtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(112)))), ((int)(((byte)(152)))));
+            this.panel1.Controls.Add(this.minimizeBtn);
+            this.panel1.Controls.Add(this.closeBtn);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(378, 38);
+            this.panel1.TabIndex = 21;
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(53)))), ((int)(((byte)(106)))));
+            this.closeBtn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.closeBtn.FlatAppearance.BorderSize = 0;
+            this.closeBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(152)))), ((int)(((byte)(181)))));
+            this.closeBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(152)))), ((int)(((byte)(181)))));
+            this.closeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeBtn.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold);
+            this.closeBtn.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.closeBtn.Location = new System.Drawing.Point(335, 0);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(43, 38);
+            this.closeBtn.TabIndex = 0;
+            this.closeBtn.Text = "X";
+            this.closeBtn.UseVisualStyleBackColor = false;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label3.Location = new System.Drawing.Point(14, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(229, 34);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "My Smart Home";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // msgBox
+            // 
+            this.msgBox.BackColor = System.Drawing.Color.White;
+            this.msgBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.msgBox.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msgBox.Location = new System.Drawing.Point(20, 261);
+            this.msgBox.Multiline = true;
+            this.msgBox.Name = "msgBox";
+            this.msgBox.ReadOnly = true;
+            this.msgBox.Size = new System.Drawing.Size(334, 96);
+            this.msgBox.TabIndex = 22;
+            this.msgBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // minimizeBtn
+            // 
+            this.minimizeBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(53)))), ((int)(((byte)(106)))));
+            this.minimizeBtn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.minimizeBtn.FlatAppearance.BorderSize = 0;
+            this.minimizeBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(152)))), ((int)(((byte)(181)))));
+            this.minimizeBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(152)))), ((int)(((byte)(181)))));
+            this.minimizeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimizeBtn.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minimizeBtn.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.minimizeBtn.Location = new System.Drawing.Point(292, 0);
+            this.minimizeBtn.Name = "minimizeBtn";
+            this.minimizeBtn.Size = new System.Drawing.Size(43, 38);
+            this.minimizeBtn.TabIndex = 1;
+            this.minimizeBtn.Text = "_";
+            this.minimizeBtn.UseVisualStyleBackColor = false;
+            this.minimizeBtn.Click += new System.EventHandler(this.minimizeBtn_Click);
+            // 
+            // Porte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(378, 359);
+            this.Controls.Add(this.msgBox);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.accessBtn);
+            this.Controls.Add(this.prenomBox);
+            this.Controls.Add(this.prenomLabel);
+            this.Controls.Add(this.nomBox);
+            this.Controls.Add(this.nomLabel);
+            this.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Porte";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Porte";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nomLabel;
+        private System.Windows.Forms.TextBox nomBox;
+        private System.Windows.Forms.TextBox prenomBox;
+        private System.Windows.Forms.Label prenomLabel;
+        private System.Windows.Forms.Button accessBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox msgBox;
+        private System.Windows.Forms.Button minimizeBtn;
+    }
+}
+
---
+++ Porte/Porte.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Server_bib;
+using Smart_Home_bib;
+
+namespace Porte
+{
+    public partial class Porte : Form
+    {
+        public Porte()
+        {
+            InitializeComponent();
+        }
+
+        private async void accessBtn_Click(object sender, EventArgs e)
+        {
+            ReceptionNotification o = (ReceptionNotification)Activator.GetObject(typeof(ReceptionNotification),
+               "tcp://localhost:8090/Envoi Notification");
+
+            if (string.IsNullOrEmpty(this.nomBox.Text) && string.IsNullOrEmpty(this.prenomBox.Text))
+                this.msgBox.Text = "Les champs ne doit pas être vide.";
+            else
+            {
+                o.sendNotification(new Notification(this.nomBox.Text, this.prenomBox.Text));
+                this.msgBox.Text = this.nomBox.Text + " " + this.prenomBox.Text + ", attendez que l'administrateur de la maison vous ouvre la porte.";
+                await Task.Delay(180000);
+                this.nomBox.Clear();
+                this.prenomBox.Clear();
+                this.msgBox.Clear();
+            }
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void minimizeBtn_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
